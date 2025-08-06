97711a115e2e1bc81a66f766a062ef331b567b47
Anupama-09 <anupamakrishnakripa@gmail.com>
2025-08-06 20:51:10 +0530

    sortsheroes.py

---
+++ sortsheroes.py
@@ -0,0 +1,17 @@
+def bubble_sort():
+    n=int(input("Enter how many heros: "))
+    names=[]
+    temp=0
+    for i in range(n):
+        name=input(f"Enter name{i+1} : ")
+        names.append(name)
+    print("Original list: ",names)
+    for i in range(n): 
+        for j in range(0, n - i - 1): 
+            if len(names[j])<len(names[j+1]):
+                temp=names[j+1]
+                names[j+1]=names[j]
+                names[j]=temp
+    print("Descending order: ",names)
+    
+bubble_sort()
